7629d737a03cb1a54d8d2e582c983ef68b87e75d
Norbert Schneider <Norbert-Schneider@gmx.de>
2012-04-14 17:34:32 +0200

    Changed download script to support travis-ci.

--- scripts/UpdateGeoIpFiles.sh
+++ scripts/UpdateGeoIpFiles.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
 
-# Download the newest archives if they have changed since the last update.
-wget -N -q http://geolite.maxmind.com/download/geoip/database/GeoLiteCity.dat.gz
-wget -N -q http://geolite.maxmind.com/download/geoip/database/asnum/GeoIPASNum.dat.gz
+echo "Change to script dir"
+cd ./scripts
+
+# Download the newest archives if they have changed since the last
+# update.
+echo "Downloading files"
+wget -N http://geolite.maxmind.com/download/geoip/database/GeoLiteCity.dat.gz
+wget -N http://geolite.maxmind.com/download/geoip/database/asnum/GeoIPASNum.dat.gz
 
 # Extract archives and move files into the resources folder
+echo "Extracting files"
 gunzip -c GeoLiteCity.dat.gz > GeoLiteCity.dat
 gunzip -c GeoIPASNum.dat.gz > GeoIPASNum.dat
 
+echo "Copying files to resources folder"
 mv ./GeoLiteCity.dat ../resources/
 mv ./GeoIPASNum.dat ../resources/
