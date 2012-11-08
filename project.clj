420d8757e26867ebb12022a46bb177fb5758f30f
Marcus Cavanaugh <m@mcav.com>
2012-11-08 13:04:45 -0800

    add data files and move to snapshot versioning

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
-(defproject clj-geoip "0.1.1"
+(defproject clj-geoip "SNAPSHOT"
   :description "Thin Clojure layer on top of the Java GeoIP API.
 Please have a look at the GeoIP homepage at http://www.maxmind.com/app/ip-location."
   :java-source-paths ["java"]
-  :dependencies [[org.clojure/clojure "1.3.0"]]
+  :dependencies [[org.clojure/clojure "1.4.0"]]
   :dev-dependencies [[lein-marginalia "0.7.0"]]
   :plugins [[lein-swank "1.4.4"]])
