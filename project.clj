28ca5481ba8a5caaabb1fd7d68398cbfbb663403
Phil Crosby <phil.crosby@gmail.com>
2012-12-28 15:17:52 -0800

    Depend on clojure 1.4

--- project.clj
+++ project.clj
@@ -2,6 +2,6 @@
   :description "Thin Clojure layer on top of the Java GeoIP API.
 Please have a look at the GeoIP homepage at http://www.maxmind.com/app/ip-location."
   :java-source-paths ["java"]
-  :dependencies [[org.clojure/clojure "1.3.0"]]
+  :dependencies [[org.clojure/clojure "1.4.0"]]
   :dev-dependencies [[lein-marginalia "0.7.0"]]
   :plugins [[lein-swank "1.4.4"]])
