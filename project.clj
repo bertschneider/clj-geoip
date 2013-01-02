f25e4996cf94f3f126d6f7fbbbc8952abbfe4190
Norbert Schneider <Norbert-Schneider@gmx.de>
2013-01-02 20:04:30 +0100

    Fixed travis build.
    Travis needs an extra parameter to use Leiningen2 and the aot property
    has to be defined in the projects.clj to compile the java files before
    the test run.

--- project.clj
+++ project.clj
@@ -1,7 +1,9 @@
 (defproject clj-geoip "0.1-SNAPSHOT"
   :description "Thin Clojure layer on top of the Java GeoIP API.
 Please have a look at the GeoIP homepage at http://www.maxmind.com/app/ip-location."
+  :min-lein-version "2.0.0"
   :java-source-paths ["java"]
+  :aot :all
   :dependencies [[org.clojure/clojure "1.4.0"]]
   :dev-dependencies [[lein-marginalia "0.7.0"]]
   :plugins [[lein-swank "1.4.4"]])
