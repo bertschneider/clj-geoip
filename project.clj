f68481e68e8ab65321c028368df208d7f8a11fbd
Sven Pedersen <sven.pedersen@gmail.com>
2014-11-01 16:59:43 -0400

    change minimum Leiningen version to 2.1.0 for Clojars deployment

--- project.clj
+++ project.clj
@@ -6,7 +6,7 @@ Please have a look at the GeoIP homepage at http://www.maxmind.com/app/ip-locati
             :url "http://www.eclipse.org/legal/epl-v10.html"
             :distribution :repo
             :comments "same as Clojure"}
-  :min-lein-version "2.0.0"
+  :min-lein-version "2.1.0"
   :aot :all
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [com.maxmind.geoip/geoip-api "1.2.14"]]
