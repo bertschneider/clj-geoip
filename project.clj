58284084b203a4de702bd4736d6377dff4fb8831
Sven Pedersen <sven.pedersen@gmail.com>
2014-11-01 15:23:26 -0400

    switch to using standard Leiningen dependencies instead of including the source update existing dependencies to latest version

--- project.clj
+++ project.clj
@@ -1,14 +1,14 @@
 (defproject clj-geoip "0.1"
   :description "Thin Clojure layer on top of the Java GeoIP API.
 Please have a look at the GeoIP homepage at http://www.maxmind.com/app/ip-location."
-  :url "https://github.com/Norrit/clj-geoip"
+  :url "https://github.com/bertschneider/clj-geoip"
   :license {:name "Eclipse Public License - v 1.0"
             :url "http://www.eclipse.org/legal/epl-v10.html"
             :distribution :repo
             :comments "same as Clojure"}
   :min-lein-version "2.0.0"
-  :java-source-paths ["java"]
   :aot :all
-  :dependencies [[org.clojure/clojure "1.4.0"]]
-  :dev-dependencies [[lein-marginalia "0.7.0"]]
-  :plugins [[lein-swank "1.4.4"]])
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [com.maxmind.geoip/geoip-api "1.2.14"]]
+  :plugins [[lein-swank "1.4.5"]
+            [lein-marginalia "0.8.0"]])
