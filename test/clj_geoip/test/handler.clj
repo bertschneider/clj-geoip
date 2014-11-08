e1556cf1d2239d80c23300a392dce6c15c80038e
Norbert Schneider <Norbert-Schneider@gmx.de>
2014-11-08 15:45:28 +0100

    Close geoip after handler test

--- test/clj_geoip/test/handler.clj
+++ test/clj_geoip/test/handler.clj
@@ -1,9 +1,12 @@
 (ns clj-geoip.test.handler
   (:require [midje.sweet :refer :all]
+            [clj-geoip.core :refer [geoip-close]]
             [clj-geoip.handler :refer :all]))
 
-(facts "about the ring handler"
+(with-state-changes [(after :facts (geoip-close))]
 
-       (fact "it adds location information to the :location key of the request map"
-             (let [handler (geoip-handler #(:location %))]
-               (handler {:remote-addr "209.85.148.100"})) => (contains {:countryName "United States"})))
+  (facts "about the ring handler"
+
+         (fact "it adds location information to the :location key of the request map"
+               (let [handler (geoip-handler #(:location %))]
+                 (handler {:remote-addr "209.85.148.100"})) => (contains {:countryName "United States"}))))
