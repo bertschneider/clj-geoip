bbd92cf89938ed904d2c9ea723e1df44b48a984d
Norbert Schneider <Norbert-Schneider@gmx.de>
2014-11-08 15:02:34 +0100

    Switch test framework to midje
    Midje can be used in the further development to mock function calls
    which can not be done with clojure.test.
    
    Resolves issue-8

--- test/clj_geoip/test/core.clj
+++ test/clj_geoip/test/core.clj
@@ -1,30 +1,25 @@
 (ns clj-geoip.test.core
-  (:use [clj-geoip.core])
-  (:use [clojure.test]))
+  (:require [midje.sweet :refer :all]
+            [clj-geoip.core :refer :all]))
 
-(deftest lookup-ip-with-geoip
-  (is (thrown? IllegalStateException (lookup "IP")))
-  (is (true? (geoip-init :IPv4)))
-  (is (true? (initialized?)))
-  (is (map? (lookup "google.com")))
-  (let [location (lookup "209.85.148.100")]
-    (is (= "209.85.148.100" (:ip location)))
-    (is (= "US" (:countryCode location)))
-    (is (= "AS15169 Google Inc." (:asn location))))
-  (is (true? (geoip-close)))
-  (is (false? (initialized?)))
-  (is (thrown? IllegalStateException (lookup "IP")))
-  (is (true? (geoip-init :IPv4 :memory))))
+(facts "about lookup"
 
-(deftest lookup-ipv6-with-geoip
-  (is (true? (geoip-init :IPv6)))
-  (is (true? (initialized? :IPv6)))
-  (is (map? (lookup "google.com" :IPv6)))
-  (let [location (lookup "2001:4860:4860::8888")]
-    (is (= "2001:4860:4860::8888" (:ip location)))
-    (is (= "US" (:countryCode location)))
-    (is (= "AS15169 Google Inc." (:asn location))))
-  (is (true? (geoip-close)))
-  (is (false? (initialized? :IPv6)))
-  (is (thrown? IllegalStateException (lookup "IP")))
-  (is (true? (geoip-init :IPv6 :memory))))
+       (fact "it has to be initialized"
+             (initialized?) => false
+             (geoip-init :IPv4+6) => true
+             (initialized? :IPv4) => true
+             (initialized? :IPv6) => true
+             (initialized? :IPv4+6) => true)
+
+       (fact "it looks up IPv4 adresses"
+             (lookup "209.85.148.100") => (contains {:ip "209.85.148.100"
+                                                    :countryCode "US"
+                                                    :asn "AS15169 Google Inc."}))
+
+       (fact "it looks up IPv6 adresses"
+             (lookup "2001:4860:4860::8888") => (contains {:ip "2001:4860:4860::8888"
+                                                          :countryCode "US"
+                                                          :asn "AS15169 Google Inc."}))
+
+       (fact "it has to be shut down"
+             (geoip-close) => true))
